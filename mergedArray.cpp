150dba824f3aa7e997e3462c765f8f0c1bafe6a3
Somay <163437756+CouchPtato@users.noreply.github.com>
2024-05-08 13:43:53 +0530

    Update and rename stringMerge.cpp to mergedArray.cpp

---
+++ mergedArray.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int* mergedSortedArray(const int* arr1, int size1, const int* arr2, int size2)
+{
+    int* mergedArray = new int[size1 + size2];
+    int i = 0, j = 0, k = 0;
+
+    while (i < size1 && j < size2)
+    {
+        if (arr1[i] < arr2[j])
+        {
+            mergedArray[k++] = arr1[i++];
+        }
+        else 
+        {
+            mergedArray[k++] = arr2[j++];
+        }
+    }
+
+    while(i < size1)
+    {
+        mergedArray[k++] = arr1[i++];
+    }
+    while(j < size2)
+    {
+        mergedArray[k++] = arr2[j++];
+    }
+    return mergedArray;
+}
+int main()
+{
+    int arr1[] = {1,3,5,7};
+    int arr2[] = {2,4,6,8,10};
+    int size1 = sizeof(arr1)/sizeof(arr1[0]);
+    int size2 = sizeof(arr2)/sizeof(arr2[0]);
+
+    int* mergedArray = mergedSortedArray(arr1, size1, arr2, size2);
+    cout << "Merged Array: ";
+    for (int i=0; i<size1+size2; ++i)
+    {
+        cout << mergedArray[i] << " ";
+    }
+    cout << endl;
+    delete[] mergedArray;
+    return 0;
+}
